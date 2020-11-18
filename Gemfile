f00497272f1716d3291cac82fad666c5fb751ef1
AB <austinbuhler@gmail.com>
2020-11-18 10:20:44 -0500

    JWT auth

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'rack-cors'
 gem 'faker'
 gem 'geocoder'
 gem 'json'
+gem 'jwt'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
+  def profile
+    render json: current_user
+  end
+
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      token = encode_token(user_id: user.id)
+      render json: {user: user, jwt: token}
+    else
+      render json: {error: 'failed to create user'}
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
