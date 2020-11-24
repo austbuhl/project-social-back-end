8e633820ab1ae3c0f1a40850dce29ab48df7ed1e
AB <austinbuhler@gmail.com>
2020-11-24 14:58:41 -0500

    Friendships creating & removing properly

---
+++ app/models/friendship.rb
@@ -0,0 +1,20 @@
+class Friendship < ApplicationRecord
+  belongs_to :user
+  belongs_to :friend, class_name: "User"
+
+  def self.friend_request(user_id, friend_id)
+    Friendship.create(user_id: user_id, friend_id: friend_id, status: 'pending')
+    Friendship.create(user_id: friend_id, friend_id: user_id, status: 'pending')
+  end
+
+  def self.accept_friend(user_id, friend_id)
+    Friendship.find_by(user_id: user_id, friend_id: friend_id).update(status: 'accepted')
+    Friendship.find_by(user_id: friend_id, friend_id: user_id).update(status: 'accepted')
+  end
+
+  def self.remove_friend(user_id, friend_id)
+    Friendship.find_by(user_id: user_id, friend_id: friend_id).destroy
+    Friendship.find_by(user_id: friend_id, friend_id: user_id).destroy
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_162307) do
+ActiveRecord::Schema.define(version: 2020_11_24_192919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,16 @@ ActiveRecord::Schema.define(version: 2020_11_20_162307) do
     t.time "time"
   end
 
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "friend_id"
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
+
   create_table "parks", force: :cascade do |t|
     t.string "name"
     t.decimal "latitude", precision: 10, scale: 6
@@ -74,4 +84,5 @@ ActiveRecord::Schema.define(version: 2020_11_20_162307) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "friendships", "users"
 end
